e53aba8474edef39b09e934123883dbb03b7eeb8
youssef <youssef.segh@hotmail.fr>
2021-12-22 00:37:47 +0100

    Collisions terminées

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { STAGE_WIDTH, checkCollision } from "../gameHelpers";
 import { randomTetromino, TETROMINOS } from "../tetrominos";
 
 export const usePlayer = () => {
@@ -26,6 +26,18 @@ export const usePlayer = () => {
     const clonedPlayer = JSON.parse(JSON.stringify(player));
     clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
 
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+
     setPlayer(clonedPlayer);
   };
 
@@ -45,5 +57,5 @@ export const usePlayer = () => {
     });
   }, []);
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
 };
